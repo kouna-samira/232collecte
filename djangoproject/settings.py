4b70328f6551e632ac4462f089029551dbe87165
kouna-samira <kounasamira3@gmail.com>
2026-04-30 22:43:15 +0100

    troisieme

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -25,10 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-22w(xc$b^je+qr2l^h!v3)+c!z8&fr2^l$u3m9qflua+o4*1s&'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-key-pour-dev')
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['web-production-1190b.up.railway.app', 'localhost', '127.0.0.1']
 
